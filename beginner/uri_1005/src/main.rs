56091fab671537866c8c28efe1c5e8ba221ab6bf
André Filipe Barranco <b.andrefilipe@gmail.com>
2021-10-23 22:11:13 -0300

    feat: Average 1

---
+++ beginner/uri_1005/src/main.rs
@@ -0,0 +1,57 @@
+use std::io;
+use std::io::{BufRead, BufReader, Read};
+
+fn main() {
+    println!("{}", solution(io::stdin()));
+}
+
+fn solution<R: Read>(reader: R) -> String {
+    let mut reader: BufReader<R> = BufReader::new(reader);
+    let a: f64 = parse_f64(get_input(&mut reader));
+    let b: f64 = parse_f64(get_input(&mut reader));
+    format!("MEDIA = {:.5}", average(a, b))
+}
+
+fn get_input<R: BufRead>(reader: &mut R) -> String {
+    let mut input: String = String::new();
+    reader.read_line(&mut input).unwrap();
+    input
+}
+
+fn parse_f64(string: String) -> f64 {
+    string.trim().parse().unwrap()
+}
+
+fn average(a: f64, b: f64) -> f64 {
+    const A_WEIGHT: f64 = 3.5;
+    let a = a * A_WEIGHT;
+
+    const B_WEIGHT: f64 = 7.5;
+    let b = b * B_WEIGHT;
+
+    (a + b) / (A_WEIGHT + B_WEIGHT)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_solution_a() {
+        let cursor = Cursor::new(b"5.0\n7.1\n");
+        assert_eq!(solution(cursor), "MEDIA = 6.43182");
+    }
+
+    #[test]
+    fn test_solution_b() {
+        let cursor = Cursor::new(b"0.0\n7.1\n");
+        assert_eq!(solution(cursor), "MEDIA = 4.84091");
+    }
+
+    #[test]
+    fn test_solution_c() {
+        let cursor = Cursor::new(b"10.0\n10.0\n");
+        assert_eq!(solution(cursor), "MEDIA = 10.00000");
+    }
+}
